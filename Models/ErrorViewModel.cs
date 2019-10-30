ee85899a3c82f77cf669d3bd2c438dc5da7d33a0
Otavio Silva <p_otavio_s@hotmail.com>
2019-10-30 01:46:09 -0300

    -

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace net_core_2._2_github_actions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
